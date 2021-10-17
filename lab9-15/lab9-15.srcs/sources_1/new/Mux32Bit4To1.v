a9b88331d7735c42fde1f646bf20ccad7362ae0b
Nathaniel Rodriguez <golansy0@gmail.com>
2021-10-16 19:52:12 -0700

    Muxes and Sign Extension

--- lab9-15/lab9-15.srcs/sources_1/new/Mux32Bit4To1.v
+++ lab9-15/lab9-15.srcs/sources_1/new/Mux32Bit4To1.v
@@ -7,17 +7,21 @@
 // Description - Performs signal multiplexing between 2 32-Bit words.
 ////////////////////////////////////////////////////////////////////////////////
 
-module Mux32Bit2To1(out, inA, inB, sel);
+module Mux32Bit2To1(out, in0, in1, in2, in3 sel);
 
     output reg [31:0] out;
     
-    input [31:0] inA;
-    input [31:0] inB;
+    input [31:0] in0;
+    input [31:0] in1;
+    input [31:0] in2;
+    input [31:0] in3;
     input sel;
 
     always @ * begin
-        if (sel == 0) out <= inA;
-        if (sel == 1) out <= inB;
+        if (sel == 0) out <= in0;
+        if (sel == 1) out <= in1;
+        if (sel == 2) out <= in2;
+        if (sel == 3) out <= in3;
     end
 
 endmodule
