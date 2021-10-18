6072234fac7bc47fd3330b332bc1ca7928c84b33
Carson <carsonhayes@email.arizona.edu>
2021-10-18 16:10:49 -0700

    commit

--- lab9-15/lab9-15.srcs/sources_1/new/ALU32Bit.v
+++ lab9-15/lab9-15.srcs/sources_1/new/ALU32Bit.v
@@ -26,112 +26,118 @@
 //   operations needed to support. 
 ////////////////////////////////////////////////////////////////////////////////
 
-module ALU32Bit(ALUControl, A, B, hi_read, lo_read, ALUResult, Zero, hi_write, lo_write);
+module ALU32Bit(ALUControl, A, B, hi_input, lo_input, ALUResult, Zero, hi_output, lo_output);
 
 	input [4:0] ALUControl; // control bits for ALU operation
                                 // you need to adjust the bitwidth as needed
 	input [31:0] A, B;	    // inputs
-    input [31:0] hi_read, lo_read;
+    input [31:0] hi_input, lo_input;
 
 	output reg [31:0] ALUResult;	// answer
 	output reg Zero;	    // Zero=1 if ALUResult == 0
 
-	output reg [31:0] hi_write, lo_write;
+	output reg [31:0] hi_output, lo_output;
 	reg [63:0] temp;
  	
     integer i, flag;
     
-    always @(ALUControl, A, B) begin
+    always @ B begin
         ALUResult = 0;
-    case (ALUControl) 
-    5'b00000 : begin //AND
-        ALUResult <= A & B;
-    end
-    5'b00001 : begin //OR
-        ALUResult <= A | B;
-    end
-    5'b00010 : begin //add
-        ALUResult <= A + B;
-    end
-    5'b00011 : begin //shift left logical
-        ALUResult <= B << A;
-    end
-    5'b00100 : begin //shift right logical
-        ALUResult <= B >> A;
-    end
-    5'b00101 : begin //multiply
-        temp <= A * B;
-        hi_write <= temp[63:32];
-        lo_write <= temp[31:0];
-		ALUResult <= lo_write;
-    end
-    5'b00110 : begin //sub
-        ALUResult <= A - B;
-    end
-    5'b00111 : begin //set less than
-        if(A < B)
-            ALUResult <= 1'b1;
-        else
-            ALUResult <= 1'b0;
-    end
-    5'b01000 : begin //madd 
-        temp <= A * B;
-		hi_write <= hi_read + temp[63:32];
-		lo_write <= lo_read + temp[31:0];    
-    end
-    5'b01001 : begin //msub  
-        temp <= A * B;
-		hi_write <= hi_read - temp[63:32];
-		lo_write <= lo_read - temp[31:0];
-    end
-	5'b01010 : begin //lui
-		ALUResult <= {B[15:0],16'b0};
-	end
-    5'b01011 : begin //mthi 
-        hi_write <= A;   
-    end
-	5'b01100 : begin //NOR
-		ALUResult <= ~(A | B);
-	end
-	5'b01101 : begin //XOR
-		ALUResult <= A ^ B;
-	end
-    5'b01110 : begin //mtlo
-        lo_write <= A;
-    end
-    5'b01111 : begin //mfhi
-       // A <= hi;
-        ALUResult <= hi_read;
-    end
-    5'b10000 : begin //mflo
-        //A <= lo;
-        ALUResult <= lo_read;
-    end
-    5'b10001 : begin //add 16-bit for sh and lh
-        ALUResult <= A[15:0] + B[15:0];
-    end
-    5'b10010 : begin //add 8-bit for lb and sb
-        ALUResult <= A[7:0] + B[7:0];
-    end
-    5'b10011 : begin //movn
-        if(B != 0)
-            ALUResult <= A;
-    end
-    5'b10100 : begin //movz
-        if(B == 0)
-            ALUResult <= A;
-    end
-    5'b10101 : begin //rotrv and rotr
-        ALUResult <= (B >> A) | (B << ~A);
-    end
-    5'b10110 : begin //sra
-        ALUResult <= B <<< A;
-    end
-    default: begin 
-        ALUResult <= ALUResult;
-    end
-     
-    endcase
+        case (ALUControl) 
+            5'b00000 : begin //AND
+                ALUResult <= A & B;
+            end
+            5'b00001 : begin //OR
+                ALUResult <= A | B;
+            end
+            5'b00010 : begin //add
+                ALUResult <= A + B;
+            end
+            5'b00011 : begin //shift left logical
+                ALUResult <= B << A;
+            end
+            5'b00100 : begin //shift right logical
+                ALUResult <= B >> A;
+            end
+            5'b00101 : begin //mult
+                temp <= A * B;
+                hi_output <= temp[63:32];
+                lo_output <= temp[31:0];
+                ALUResult <= lo_output;
+            end
+            5'b00110 : begin //sub
+                ALUResult <= A - B;
+            end
+            5'b00111 : begin //set less than
+                if($signed(A) < $signed(B))
+                    ALUResult <= 1'b1;
+                else
+                    ALUResult <= 1'b0;
+            end
+            5'b01000 : begin //madd 
+                temp <= A * B;
+                temp <= {hi_input, lo_input} + temp; 
+                hi_output <= temp[63:32];
+                lo_output <= temp[31:0];    
+            end
+            5'b01001 : begin //msub  
+                temp <= A * B;
+                temp <= {hi_input, lo_input} - temp; 
+                hi_output <= temp[63:32];
+                lo_output <= temp[31:0];
+            end
+            5'b01010 : begin //lui
+                ALUResult <= {B[15:0],16'b0};
+            end
+            5'b01011 : begin //mthi 
+                hi_output <= A;   
+            end
+            5'b01100 : begin //NOR
+                ALUResult <= ~(A | B);
+            end
+            5'b01101 : begin //XOR
+                ALUResult <= A ^ B;
+            end
+            5'b01110 : begin //mtlo
+                lo_output <= A;
+            end
+            5'b01111 : begin //mfhi
+                ALUResult <= hi_input;
+            end
+            5'b10000 : begin //mflo
+                ALUResult <= lo_input;
+            end
+            5'b10001 : begin //add 16-bit for sh and lh
+                ALUResult <= A[15:0] + B[15:0];
+            end
+            5'b10010 : begin //add 8-bit for lb and sb
+                ALUResult <= A[7:0] + B[7:0];
+            end
+            5'b10011 : begin //pass A
+                ALUResult <= A;
+            end
+            5'b10100 : begin //sltu
+                if(A < B)
+                    ALUResult <= 1'b1;
+                else
+                    ALUResult <= 1'b0;
+            end
+            5'b10101 : begin //rotr
+                ALUResult <= (B >> A) | (B << ~A);
+            end
+            5'b10110 : begin //sra
+                ALUResult <= B >>> A;
+            end
+            5'b10111 : begin //seh
+                ALUResult <= {{16{B[15]}}, B[15:0]};
+            end
+            5'b11000 : begin //seb
+                ALUResult <= {{24{B[7]}}, B[7:0]};
+            end
+            default: begin 
+                ALUResult <= ALUResult;
+            end
+        endcase
     end
 
     always @(ALUResult) begin
--- lab9-15/lab9-15.srcs/sources_1/new/HiLoRegisters.v
+++ lab9-15/lab9-15.srcs/sources_1/new/HiLoRegisters.v
@@ -20,91 +20,25 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 
-module HiLoRegisters(Clk, hi_toALU, lo_toALU, ALUControl, hi_fromALU, lo_fromALU);
-    
+module HiLoRegisters(Clk, HiWrite, LoWrite, hi_input, lo_input, hi_output, lo_output);
+    //inputs of this module are outputs of alu and vise versa
     input Clk;
-    output reg [31:0] hi_toALU, lo_toALU;
-    input [4:0] ALUControl;
-    input [31:0] hi_fromALU, lo_fromALU;
-    reg [31:0] hi_fromALU_reg, lo_fromALU_reg;
+    input [31:0] hi_input, lo_input;
+    input HiWrite, LoWrite;
+    output reg [31:0] hi_output, lo_output;
 
-    reg [31:0] regFile_hi, regFile_lo;
-    reg Write_lo, Write_hi, Read_lo, Read_hi;
+    reg [31:0] reg_hi, reg_lo;
 
     initial begin
-        regFile_hi <= 32'b0;
-        regFile_lo <= 32'b0;
-    end
-
-    always begin
-
-        case(ALUControl)
-        5'b00101 : begin //multiply
-            Write_lo <= 1'b1;
-            Write_hi <= 1'b1;
-            Read_hi <= 1'b0;
-            Read_lo <= 1'b0;
-        end
-        5'b01000 : begin //madd
-            Write_lo <= 1'b1;
-            Write_hi <= 1'b1;
-            Read_hi <= 1'b1;
-            Read_lo <= 1'b1;
-        end
-        5'b01001 : begin //msub
-            Write_lo <= 1'b1;
-            Write_hi <= 1'b1;
-            Read_hi <= 1'b1;
-            Read_lo <= 1'b1;
-        end
-        5'b01011 : begin //mthi
-            Write_lo <= 1'b0;
-            Write_hi <= 1'b1;
-            Read_hi <= 1'b0;
-            Read_lo <= 1'b0;
-        end
-        5'b01110 : begin //mtlo
-            Write_lo <= 1'b1;
-            Write_hi <= 1'b0;
-            Read_hi <= 1'b0;
-            Read_lo <= 1'b0;
-        end
-        5'b01111 : begin //mfhi
-            Write_lo <= 1'b0;
-            Write_hi <= 1'b0;
-            Read_hi <= 1'b1;
-            Read_lo <= 1'b0;
-        end
-        5'b10000 : begin //mflo
-            Write_lo <= 1'b0;
-            Write_hi <= 1'b0;
-            Read_hi <= 1'b0;
-            Read_lo <= 1'b1;
-        end
-        default : begin
-            Write_lo <= 1'b0;
-            Write_hi <= 1'b0;
-            Read_hi <= 1'b0;
-            Read_lo <= 1'b0;
-        end
-        endcase
+        reg_hi <= 32'b0;
+        reg_lo <= 32'b0;
     end
 
     always @ (posedge Clk) begin
-        hi_fromALU_reg <= hi_fromALU;
-        lo_fromALU_reg <= lo_fromALU;
-        if(Write_lo == 1) begin
-            lo_fromALU_reg <= regFile_lo;
-        end  
-        if(Write_hi == 1) begin
-            hi_fromALU_reg <= regFile_hi;
-        end
-        if(Read_hi == 1) begin
-            regFile_hi <= hi_toALU;
-        end
-        if(Read_lo == 1) begin
-            regFile_lo <= lo_toALU;
-        end
+        hi_output <= reg_hi;
+        lo_output <= reg_lo;
+        if (HiWrite == 1) reg_hi <= hi_input;
+        if (LoWrite == 1) reg_lo <= lo_input;
     end
 
 endmodule
