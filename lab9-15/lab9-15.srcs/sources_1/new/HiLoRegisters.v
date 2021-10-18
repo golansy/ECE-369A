f74b9265b52344f518472dddeda12a105fa35e38
Nathaniel Rodriguez <golansy0@gmail.com>
2021-10-17 19:34:53 -0700

    Modified HiLo

--- lab9-15/lab9-15.srcs/sources_1/new/HiLoRegisters.v
+++ lab9-15/lab9-15.srcs/sources_1/new/HiLoRegisters.v
@@ -27,30 +27,18 @@ module HiLoRegisters(Clk, HiWrite, LoWrite, hi_input, lo_input, hi_output, lo_ou
     input HiWrite, LoWrite;
     output reg [31:0] hi_output, lo_output;
 
-    reg [31:0] regFile_hi, regFile_lo;
-    reg [31:0] reg_hi_input, reg_lo_input;
+    reg [31:0] reg_hi, reg_lo;
 
     initial begin
-        regFile_hi <= 32'b0;
-        regFile_lo <= 32'b0;
+        reg_hi <= 32'b0;
+        reg_lo <= 32'b0;
     end
 
     always @ (posedge Clk) begin
-        reg_hi_input <= hi_input; //converting input to a reg
-        reg_lo_input <= lo_input; //
-        case(ALUControl)
-        5'b00101 : begin //multiply
-            reg_hi_input <= regFile_hi;
-            reg_lo_input <= regFile_lo;
-        end
-        5'b01000 : begin //madd
-            hi_output = regFile_hi;
-            lo_output = regFile_lo;
-            reg_hi_input = regFile_hi;
-            reg_lo_input = regFile_lo;
-        end
-        endcase
-
+        hi_output <= reg_hi;
+        lo_output <= reg_lo;
+        if (HiWrite == 1) reg_hi <= hi_input;
+        if (LoWrite == 1) reg_lo <= lo_input;
     end
 
 endmodule
