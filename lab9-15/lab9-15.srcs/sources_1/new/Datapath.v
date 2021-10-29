7870411505c7a7d7bbac65eef1533c7dc3868b40
Carson <carsonhayes@email.arizona.edu>
2021-10-28 18:45:27 -0700

    Changes for Lab 15 spcs

--- lab9-15/lab9-15.srcs/sources_1/new/Datapath.v
+++ lab9-15/lab9-15.srcs/sources_1/new/Datapath.v
@@ -24,14 +24,14 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 
-module Datapath(Clk, Reset, Program_Counter, WriteData, hi_output, lo_output);
+module Datapath(Clk, Reset, out7, en_out);
     input Clk, Reset;
     wire [31:0] PCAddress, PCAddress_ID, PCAddress_EX, PCAddress_shift, PCAddress_MEM;
-    /*(* MARK_DEBUG = "TRUE" *)*/output wire [31:0] Program_Counter;
+    /*(* MARK_DEBUG = "TRUE" *)*/ wire [31:0] Program_Counter;
     wire [31:0] PCPlus4, PCPlus4_ID, PCPlus4_EX, PCPlus4_MEM, PCPlus8, PCPlus8_WB;
     wire [31:0] Instruction, Instruction_ID;
     wire [4:0] WriteReg_EX, WriteReg_MEM, WriteReg_WB;
-    /*(* MARK_DEBUG = "TRUE" *)*/output wire [31:0] WriteData;
+    /*(* MARK_DEBUG = "TRUE" *)*/wire [31:0] WriteData;
     wire [31:0] rs, rt, rs_EX, rt_EX, rs_MEM, rt_MEM;
     wire RegWrite, RegWrite_EX, RegWrite_control, RegWrite_MEM, RegWrite_WB;
     wire ALUSrcB, ALUSrcB_EX;
@@ -58,7 +58,7 @@ module Datapath(Clk, Reset, Program_Counter, WriteData, hi_output, lo_output);
     wire [31:0] shift2_out;
     wire [4:0] ALUCntrl;
     wire [31:0] ALU_A, ALU_B;
-    /*(* MARK_DEBUG = "TRUE" *)*/output wire [31:0] hi_output, lo_output;
+    /*(* MARK_DEBUG = "TRUE" *)*/ wire [31:0] hi_output, lo_output;
     wire [31:0] hi_input, lo_input;
     wire [31:0] ALUResult, ALUResult_MEM, ALUResult_WB;
     wire [31:0] ReadData, ReadData_WB;
@@ -67,16 +67,22 @@ module Datapath(Clk, Reset, Program_Counter, WriteData, hi_output, lo_output);
     wire [27:0] ShiftedAddress;
     wire Zero, Zero_MEM;
     wire PCSrc;
+    wire ClkDivOut;
+
+    output [6:0] out7; //seg a, b, ... g
+    output [7:0] en_out;
+
+    ClkDiv cd0(Clk, Reset, ClkDivOut);
     
-    ProgramCounter pc(PCAddress, Program_Counter, Reset, Clk);
+    ProgramCounter pc(PCAddress, Program_Counter, Reset, ClkDivOut);
     InstructionMemory im(Program_Counter, Instruction);
     PCAdder pc_adder(Program_Counter, PCPlus4);
-    IFIDReg if_id(Clk, Reset, PCPlus4, Instruction, PCAddress_ID, Instruction_ID, PCPlus4_ID);
+    IFIDReg if_id(ClkDivOut, Reset, PCPlus4, Instruction, PCAddress_ID, Instruction_ID, PCPlus4_ID);
     
-    RegisterFile reg_file(Instruction_ID[25:21], Instruction_ID[20:16], WriteReg_WB, WriteData, RegWrite_WB, Clk, rs, rt);
+    RegisterFile reg_file(Instruction_ID[25:21], Instruction_ID[20:16], WriteReg_WB, WriteData, RegWrite_WB, ClkDivOut, rs, rt);
     Control control(Instruction_ID, PCSrc, RegWrite, ZeroExt, ALUSrcB, ALUSrcA, RegDst, Branch, MemWrite, MemRead, MemToReg, HiWrite, LoWrite, Bne, Beq, Bgez, Bltz, Bgtz, Blez, ALUOp, Jump, Jr);
     SignExtension sign_ext(Instruction_ID[15:0], ZeroExt, Immediate);
-    IDEXReg id_ex(Clk, Reset, RegWrite, ALUSrcB, ALUSrcA, RegDst, Branch, MemWrite, MemRead, MemToReg, HiWrite, LoWrite, Bne, Beq, Bgez, Bltz, Bgtz, Blez, PCAddress_ID, rs, rt, Immediate, ALUOp, Instruction_ID[10:6], Instruction_ID[20:16], Instruction_ID[15:11], PCPlus4_ID, Jr, Jump, {PCPlus4_ID[31:28], (Instruction_ID[25:0] << 2), 2'b0}, RegWrite_EX, ALUSrcB_EX, ALUSrcA_EX, RegDst_EX, Branch_EX, MemWrite_EX, MemRead_EX, MemToReg_EX, HiWrite_EX, LoWrite_EX, Bne_EX, Beq_EX, Bgez_EX, Bltz_EX, Bgtz_EX, Blez_EX, PCAddress_EX, rs_EX, rt_EX, Immediate_EX, ALUOp_EX, shamt_EX, rt_reg_EX, rd_reg_EX, PCPlus4_EX, Jr_EX, Jump_EX, JumpAddress);
+    IDEXReg id_ex(ClkDivOut, Reset, RegWrite, ALUSrcB, ALUSrcA, RegDst, Branch, MemWrite, MemRead, MemToReg, HiWrite, LoWrite, Bne, Beq, Bgez, Bltz, Bgtz, Blez, PCAddress_ID, rs, rt, Immediate, ALUOp, Instruction_ID[10:6], Instruction_ID[20:16], Instruction_ID[15:11], PCPlus4_ID, Jr, Jump, {PCPlus4_ID[31:28], (Instruction_ID[25:0] << 2), 2'b0}, RegWrite_EX, ALUSrcB_EX, ALUSrcA_EX, RegDst_EX, Branch_EX, MemWrite_EX, MemRead_EX, MemToReg_EX, HiWrite_EX, LoWrite_EX, Bne_EX, Beq_EX, Bgez_EX, Bltz_EX, Bgtz_EX, Blez_EX, PCAddress_EX, rs_EX, rt_EX, Immediate_EX, ALUOp_EX, shamt_EX, rt_reg_EX, rd_reg_EX, PCPlus4_EX, Jr_EX, Jump_EX, JumpAddress);
     
     Shifter shift2(Immediate_EX, 2, shift2_out);
     Adder pc_adder2(PCAddress_EX, shift2_out, PCAddress_shift);
@@ -85,17 +91,19 @@ module Datapath(Clk, Reset, Program_Counter, WriteData, hi_output, lo_output);
     ALUControl alu_control(ALUOp_EX, RegWrite_EX, Immediate_EX[5:0], rt_EX, ALUCntrl, RegWrite_control);
     Mux32Bit2To1 alu_src_a(ALU_A, rs_EX, {27'b0, shamt_EX}, ALUSrcA_EX);
     Mux32Bit2To1 alu_src_b(ALU_B, rt_EX, Immediate_EX, ALUSrcB_EX);
-    ALU32Bit alu(Clk, ALUCntrl, ALU_A, ALU_B, hi_output, lo_output, ALUResult, Zero, hi_input, lo_input);
-    HiLoRegisters hi_lo(Clk, HiWrite_EX, LoWrite_EX, hi_input, lo_input, hi_output, lo_output);
-    EXMEMReg ex_mem(Clk, Reset, Zero, RegWrite_control, Branch_EX, MemWrite_EX, MemRead_EX, MemToReg_EX, Bne_EX, Beq_EX, Bgez_EX, Bltz_EX, Bgtz_EX, Blez_EX, PCAddress_shift, ALUResult, rs_EX, rt_EX, WriteReg_EX, PCPlus4_EX, Jump_EX, Jr_Mux_out, Zero_MEM, RegWrite_MEM, Branch_MEM, MemWrite_MEM, MemRead_MEM, MemToReg_MEM, Bne_MEM, Beq_MEM, Bgez_MEM, Bltz_MEM, Bgtz_MEM, Blez_MEM, ALUResult_MEM, PCAddress_MEM, rs_MEM, rt_MEM, WriteReg_MEM, PCPlus4_MEM, Jump_MEM, JumpAddress_MEM);
+    ALU32Bit alu(ClkDivOut, ALUCntrl, ALU_A, ALU_B, hi_output, lo_output, ALUResult, Zero, hi_input, lo_input);
+    HiLoRegisters hi_lo(ClkDivOut, HiWrite_EX, LoWrite_EX, hi_input, lo_input, hi_output, lo_output);
+    EXMEMReg ex_mem(ClkDivOut, Reset, Zero, RegWrite_control, Branch_EX, MemWrite_EX, MemRead_EX, MemToReg_EX, Bne_EX, Beq_EX, Bgez_EX, Bltz_EX, Bgtz_EX, Blez_EX, PCAddress_shift, ALUResult, rs_EX, rt_EX, WriteReg_EX, PCPlus4_EX, Jump_EX, Jr_Mux_out, Zero_MEM, RegWrite_MEM, Branch_MEM, MemWrite_MEM, MemRead_MEM, MemToReg_MEM, Bne_MEM, Beq_MEM, Bgez_MEM, Bltz_MEM, Bgtz_MEM, Blez_MEM, ALUResult_MEM, PCAddress_MEM, rs_MEM, rt_MEM, WriteReg_MEM, PCPlus4_MEM, Jump_MEM, JumpAddress_MEM);
 
     CombLogicForBranching branch_logic(Zero_MEM, Bne_MEM, Beq_MEM, Bgez_MEM, Bltz_MEM, Bgtz_MEM, Blez_MEM, rs_MEM[31], Branch_MEM, PCSrc);
-    DataMemory data_mem(ALUResult_MEM, rt_MEM, Clk, MemWrite_MEM, MemRead_MEM, ReadData);
+    DataMemory data_mem(ALUResult_MEM, rt_MEM, ClkDivOut, MemWrite_MEM, MemRead_MEM, ReadData);
     PCAdder pc_adder_8(PCPlus4_MEM, PCPlus8);
-    MEMWBReg mem_wb(Clk, Reset, RegWrite_MEM, MemToReg_MEM, ALUResult_MEM, ReadData, WriteReg_MEM, PCPlus8, RegWrite_WB, MemToReg_WB, ALUResult_WB, ReadData_WB, WriteReg_WB, PCPlus8_WB);
+    MEMWBReg mem_wb(ClkDivOut, Reset, RegWrite_MEM, MemToReg_MEM, ALUResult_MEM, ReadData, WriteReg_MEM, PCPlus8, RegWrite_WB, MemToReg_WB, ALUResult_WB, ReadData_WB, WriteReg_WB, PCPlus8_WB);
     
     Mux32Bit4To1 mem_to_reg(WriteData, ReadData_WB, ALUResult_WB, PCPlus8_WB, 0, MemToReg_WB);
     Mux32Bit2To1 jump_mux(JumpMux_out, PCPlus4, JumpAddress_MEM, Jump_MEM);
     Mux32Bit2To1 pc_src(PCAddress, JumpMux_out, PCAddress_MEM, PCSrc);
+
+    Two4DigitDisplay d0(Clk, WriteData[15:0], Program_Counter[15:0], out7, en_out);
 
 endmodule
